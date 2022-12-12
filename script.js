1d47bbc55c22c7a51811247b10f0310c97f58dda
Cosmin <72227593+carpcosmin@users.noreply.github.com>
2022-12-12 10:36:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const hamburger = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+hamburger.addEventListener('click', function(){
+  this .classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+});
+
+document.querySelector("#show-log-in-popup").addEventListener("click", function(){
+  document.querySelector(".login-popup").classList.add("active"); 
+});
+document.querySelector(".login-popup .btnClose").addEventListener("click", function(){
+  document.querySelector(".login-popup").classList.remove("active"); 
+}); 
+
+document.querySelector("#show-sign-in-popup").addEventListener("click", function(){
+  document.querySelector(".signin-popup").classList.add("active"); 
+});
+document.querySelector(".signin-popup .btnClose").addEventListener("click", function(){
+  document.querySelector(".signin-popup").classList.remove("active"); 
+}); 
+
+// Code for the search bar
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+
+window.onscroll = () =>{
+  searchBtn.classList.remove('fa-times');
+  searchBar.classList.remove('active');
+}
+
+searchBtn.addEventListener('click', () =>{
+  searchBtn.classList.toggle('fa-times');
+  searchBar.classList.toggle('active');
+});
+
+//Code for Home section
+let videoBtn = document.querySelectorAll('.vid-btn');
+videoBtn.forEach(btn =>{
+  btn.addEventListener('click', ()=>{
+      document.querySelector('.controls .active').classList.remove('active');
+      btn.classList.add('active');
+      let src = btn.getAttribute('data-src');
+      document.querySelector('#video-slider').src = src;
+  });
+});
