c14b25d89581c999108f427a480acee7d79675b1
Cosmin <72227593+carpcosmin@users.noreply.github.com>
2023-01-07 17:27:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["index.html", "style.css", "res/logo_S.png"]);
+            return cache.addAll(["index.html", "style.css", "res/logo_S.png", "res/book-icon.png"]);
         })
     );
 });
